0b8eff591bafd5f2d09fdc0c06ba4bc35b606bc0
peterstastny <peter.stastny7@gmail.com>
2021-09-06 16:17:02 +0200

    nameparts done

---
+++ name-parts/nameparts.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let fullname = "Allan Smith Jones";
+
+getNameParts(fullname);
+
+function getNameParts(nameParam) {
+  let firstName = nameParam.substring(0, nameParam.indexOf(" "));
+  let middleName = nameParam.substring(nameParam.indexOf(" ") + 1, nameParam.lastIndexOf(" "));
+  let lastName = nameParam.substring(nameParam.lastIndexOf(" ") + 1);
+  console.log({ firstName, middleName, lastName });
+}
